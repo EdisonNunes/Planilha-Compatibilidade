b03f797cfd6612faaf7d23a212ba730bd89a88ce
Edison Nunes <edison.nunes@outlook.com.br>
2025-04-01 15:48:39 -0300

    first commit

---
+++ compatibilidade_quimica.py
@@ -0,0 +1,186 @@
+import streamlit as st
+#from data_loader import carregar_dados
+import datetime
+
+#base = carregar_dados()
+
+format_1casa='%0.1f'
+format_2casas='%0.2f'
+format_3casas='%0.3f'
+
+st.write('## Planilha de Compatibilidade Química')
+container1 = st.container(border=True)
+with container1:
+    coluna_esquerda, coluna_meio, coluna_direita = st.columns([1,1,1])
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        cat_membr = st.text_input('Categoria da Membrana', max_chars= 12)
+    with col2:   
+        lote1 = st.text_input('Lote 1', max_chars= 10) 
+        lote2 = st.text_input('Lote 2', max_chars= 10) 
+        lote3 = st.text_input('Lote 3', max_chars= 10) 
+    with col3:   
+        serial1 = st.text_input('Serial 1', max_chars= 5) 
+        serial2 = st.text_input('Serial 2', max_chars= 5) 
+        serial3 = st.text_input('Serial 3', max_chars= 5)   
+
+container2 = st.container(border=True)
+with container2:
+    coluna_esquerda, coluna_meio, coluna_direita = st.columns([1,1,1])
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        poro_cat_membr = st.text_input('Poro', max_chars= 12)
+    with col2:   
+        fabri = st.text_input('Fabricante', max_chars= 10) 
+    with col3:   
+        linha = st.text_input('Linha', max_chars= 15) 
+        
+
+container3 = st.container(border=True)
+with container3:
+    coluna_esquerda, coluna_meio, coluna_direita = st.columns([1,1,1])
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        cat_disp = st.text_input('Categoria Dispositivo', max_chars= 12)
+        poro_cat_disp = st.text_input('Poro Dispositivo µm', max_chars= 12)
+    with col2:
+        lote_disp = st.text_input('Lote Dispositivo', max_chars= 10)   
+        fabri_disp = st.text_input('Fabricante Dispositivo', max_chars= 10) 
+    with col3: 
+        serial_cat_disp = st.text_input('Serial Dispositivo', max_chars= 6)  
+        linha_cat_disp = st.text_input('Linha Dispositivo', max_chars= 15) 
+
+container4 = st.container(border=True)
+with container4:
+    produto = st.text_input('Produto', max_chars= 50)
+       
+container5 = st.container(border=True)
+with container5:
+    coluna_esquerda, coluna_meio, coluna_direita = st.columns([1,1,1])
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        temp_filtra = st.text_input('Temperatura de Filtração', max_chars= 12)
+    with col2:   
+        manu_temp = st.text_input('Manutenção de Temperatura', max_chars= 20) 
+    with col3:   
+        tmp_contato = st.text_input('Tempo de contato', max_chars= 10)   
+
+container6 = st.container(border=True)
+with container6:
+    coluna_esquerda, coluna_meio, coluna_direita = st.columns([1,1,1])
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        id_sala = st.text_input('ID Sala', max_chars= 10)
+    with col2:   
+        sala_temp = st.text_input('Temperatura', max_chars= 20) 
+    with col3:   
+        sala_umid = st.text_input('Umidade', max_chars= 10)      
+
+
+st.markdown('<div style="text-align: center;"><h3>Pesagem Membrana 47 mm</h3></div>', unsafe_allow_html=True)
+container7 = st.container(border=True)
+with container7:
+    coluna_1, coluna_2, coluna_3, coluna_4 = st.columns([1,1,1,1])
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        st.markdown('<div style="text-align: center;"><h5>Peso Inicial(g)</h5></div>', unsafe_allow_html=True,
+                    help='Pesar as membranas antes da realização dos testes iniciais')
+    with col2:
+        pi_memb_1 = st.number_input('Membrana Inicial 1', format=format_3casas)   
+        pi_memb_2 = st.number_input('Membrana Inicial 2', format=format_3casas) 
+        pi_memb_3 = st.number_input('Membrana Inicial 3', format=format_3casas)
+    with col3:   
+        st.markdown('<div style="text-align: center;"><h5>Peso Final(g)</h5></div>', unsafe_allow_html=True,
+                    help='Pesar as membranas após a realização dos testes') 
+    with col4:   
+        pf_memb_1 = st.number_input('Membrana Final 1', format=format_3casas)
+        pf_memb_2 = st.number_input('Membrana Final 2', format=format_3casas) 
+        pf_memb_3 = st.number_input('Membrana Final 3', format=format_3casas) 
+
+
+st.markdown('<div style="text-align: center;"><h3>Cálculo de Fluxo - Pré molhagem 100 ml</h3></div>', unsafe_allow_html=True)
+container8 = st.container(border=True)
+with container8:
+    coluna_1, coluna_2, coluna_3, coluna_4 = st.columns([1,1,1,1])
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        st.markdown('<div style="text-align: center;"><h5>Fluxo Inicial Tempo do Cronometro</h5></div>', unsafe_allow_html=True,
+                    help='Pré molhar as membranas com 100mL de WFI, em seguida com + 100mL cronometrar o tempo de filtragem\nBomba de vácuo a 20Hg'
+                    )
+    with col2:   
+        fli_memb_1 = st.number_input('Membr 1 Inic - 100 ml', format=format_2casas)
+        fli_memb_2 = st.number_input('Membr 2 Inic - 100 ml', format=format_2casas) 
+        fli_memb_3 = st.number_input('Membr 3 Inic - 100 ml', format=format_2casas)
+    with col3:   
+        st.markdown('<div style="text-align: center;"><h5>Fluxo Final Tempo do Cronometro</h5></div>', unsafe_allow_html=True,
+                    help='Após o enxague e calculo de fluxo final, realizar teste de Integridade final') 
+    with col4:   
+        flf_memb_1 = st.number_input('Membr 1 Final - 100 ml', format=format_2casas)
+        flf_memb_2 = st.number_input('Membr 2 Final - 100 ml', format=format_2casas) 
+        flf_memb_3 = st.number_input('Membr 3 Final - 100 ml', format=format_2casas)
+
+st.markdown('<div style="text-align: center;"><h3>Teste de Integridade</h3></div>', unsafe_allow_html=True) 
+container9 = st.container(border=True)
+with container9:
+    coluna_1, coluna_2, coluna_3 = st.columns([1,1,1])
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        pb_padrao = st.number_input('PB Padrão', format=format_2casas)  
+
+container10 = st.container(border=True)
+with container10:
+    coluna_1, coluna_2, coluna_3, coluna_4 = st.columns([1,1,1,1])
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        st.markdown('<div style="text-align: center;"><h5>PB Fluido Padrão</h5></div>', unsafe_allow_html=True,
+                    help='Após a etapa anterior realizar diretamente o teste de PB ou molhar a membrana com 200mL de WFI e realizar o PB'
+                    )
+    with col2:   
+        memb_1_fr = st.number_input('Membr 1 Fluido Padrão', format=format_1casa, step=0.1)
+        memb_2_fr = st.number_input('Membr 2 Fluido Padrão', format=format_1casa, step=0.1) 
+        memb_3_fr = st.number_input('Membr 3 Fluido Padrão', format=format_1casa, step=0.1)
+    with col3:   
+        st.markdown('<div style="text-align: center;"><h5>PB Produto</h5></div>', unsafe_allow_html=True,
+                    help='Criar um teste de PB para o teste com Produto (<10psi >10PSI), calcular o RPB - Ao final realizar Analise Visual') 
+    with col4:   
+        memb_1_pr = st.number_input('Membr 1 Produto', format=format_1casa, step=0.1)
+        memb_2_pr = st.number_input('Membr 2 Produto', format=format_1casa, step=0.1) 
+        memb_3_pr = st.number_input('Membr 3 Produto', format=format_1casa, step=0.1)             
+        
+container11 = st.container(border=True)
+with container11:
+    coluna_1, coluna_2 = st.columns([1,1])
+    col1, col2 = st.columns(2)
+    with col1:
+        pb_prod = st.number_input('PB Produto > PB estimado', format=format_1casa, step=0.1)    
+
+container12 = st.container(border=True)
+with container12:
+    coluna_1, coluna_2 = st.columns([1,1])
+    col1, col2 = st.columns(2)
+    with col1:
+        st.markdown('<div style="text-align: center;"><h5>PB Fluido Padrão TI-Final</h5></div>', unsafe_allow_html=True,
+                    help='Após o teste de integridade final, secar a membrana e realizar a pesagem final'
+                    )  
+    with col2:   
+        fp_memb_1 = st.number_input('Membrana 1 Fluido Padrão', format=format_1casa, step=0.1)
+        fp_memb_2 = st.number_input('Membrana 2 Fluido Padrão', format=format_1casa, step=0.1) 
+        fp_memb_3 = st.number_input('Membrana 3 Fluido Padrão', format=format_1casa, step=0.1)    
+
+
+st.markdown('<div style="text-align: center;"><h3>Contato com o Produto</h3></div>', 
+            help='Molhagem em looping de todos os elementos (5min para cada um) e iniciar a contagem de contato',
+            unsafe_allow_html=True) 
+
+container13 = st.container(border=True)
+with container13:
+    coluna_1, coluna_2 = st.columns([1,1])
+    col1, col2 = st.columns(2)
+    hoje = datetime.datetime.now()
+    
+    with col1:
+        dt_inicial = st.date_input('Data Inicial',hoje, format='DD-MM-YYYY')
+        hr_inicial = st.time_input('Hora Inicial',value='now', step=60) 
+    with col2:   
+        dt_final = st.date_input('Data Final',hoje, format='DD-MM-YYYY')
+        hr_final = st.time_input('Hora Final',value='now', step=60)                 
---
+++ main.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import pandas as pd
+#import streamlit_authenticator as stauth
+#from models import session, Usuario
+##### Aula https://portalhashtag.com/cursos/1728736353758x643060883862200700
+# lista_usuarios = session.query(Usuario).all()
+
+
+# senhas_criptografadas = stauth.Hasher(['123456','123123','456456']).generate()
+
+# credenciais = { 'usernames':{
+#    'leandro@gmail.com': {'name': 'Leandro', 'password': senhas_criptografadas[0]},
+#    'edison@gmail.com': {'name': 'Edison', 'password': senhas_criptografadas[1]},
+#    'giane@gmail.com': {'name': 'Giane', 'password': senhas_criptografadas[2]},
+# }
+# }
+
+# # credenciais = {'usernames': 
+# #                {
+# #                    usuario.email: {'name': usuario.nome, 'password': usuario.senha} for usuario in lista_usuarios
+# #                }
+
+# # }
+
+# # autenticador (credenciais, nome do cookie no navegador, chave secreta, n° dias sem precisar reautenticar )
+# autenticator =stauth.Authenticate(credenciais, 'credenciais_hashco', 'fsyfus%$67fs76AH7', cookie_expiry_days=30)
+
+# def autenticar_usuario(autenticator):
+#     nome, status_autenticacao, username = autenticator.login()
+    
+#     if status_autenticacao:
+#         return {'nome': nome, 'username': username}
+#     elif status_autenticacao == False:
+#         st.error('Usuário ou senha inválidos')
+#     else:
+#         st.error('Preencha o formulário para fazer login')    
+
+
+# def logout():
+#     autenticator.logout()
+
+
+# # autenticar usuário
+# # 
+# dados_usuario = autenticar_usuario(autenticator)    
+
+
+dados_usuario = {
+    'username' : 'Edison'
+
+}
+
+#if dados_usuario:
+
+    
+    # email_usuario = dados_usuario['username']
+    # usuario =  session.query(Usuario).filter_by(email=email_usuario).first()
+
+# if usuario.admin:
+pg = st.navigation(
+    {   #     st.Page([ pagina/função, titulo])           
+        'Home': [st.Page('homepage.py', title='Compatibilidade Química')],
+        'Planilhas': [st.Page('compatibilidade_quimica.py', title='Planilhas'), 
+                    st.Page('relat_compatibilidade.py', title='Prévia Relatório')
+                    ],
+        # 'Conta': [st.Page(logout, title='Sair'),
+        #         st.Page('criar_conta.py', title='Criar Conta')
+        #         ]
+        #'Conta': [st.Page('criar_conta.py', title='Criar Conta')
+        #        ]        
+    }
+)
+# else:
+#     pg = st.navigation(
+#     {   #     st.Page([ pagina/função, titulo])           
+#         'Home': [st.Page('homepage.py', title='Hash&Co')],
+#         'Dashboards': [st.Page('dashboard.py', title='Dashboard'), 
+#                        st.Page('indicadores.py', title='Indicadores')
+#                        ],
+#         'Conta': [st.Page(logout, title='Sair')
+#                   ]
+#     }
+# )    
+
+pg.run()
