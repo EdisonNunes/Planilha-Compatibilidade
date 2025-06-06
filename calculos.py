7c06d3505ed5a13ee2d4ffd0e330ff1fcf63a3b7
Edison Nunes <edison.nunes@outlook.com.br>
2025-06-06 09:39:50 -0300

    Versão 4.10 Hora Local

--- calculos.py
+++ calculos.py
@@ -64,6 +64,23 @@
 # Resultado Var. Vazão Membrana 3	=SE(ABS((G25-D25)/D25)*100 <=N25;"aprovado"; "reprovado")
 
 import pandas as pd
+import pytz
+import datetime
+
+
+# import datetime
+# import pytz
+
+# fuso_horario = pytz.timezone('America/Sao_Paulo')
+# data_hora_local = datetime.datetime.now(fuso_horario)
+
+# st.write(f"Data e hora em São Paulo: {data_hora_local.strftime('%Y-%m-%d %H:%M:%S')}")
+
+
+def GetHoraLocal(zona):
+    fuso_horario = pytz.timezone(zona)
+    return datetime.datetime.now(fuso_horario)
+
 
 def string_para_float(tempo_str):
     """
